bee432c09a46bc8a49c75a65f0f2752ac95fc831
33284祐介 小林 <33284@ojs.ac.jp>
2022-04-28 15:06:44 +0900

    メソッドの移動

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SalesCalculator {
-  public class SalesCounter {
+    public class SalesCounter {
         private List<Sale> _sales; //csvファイルから読み込んだデータ
 
 
@@ -15,15 +16,31 @@ namespace SalesCalculator {
         }
 
         //店舗売り上げを求める
-        public Dictionary<string,int> GetPerStoreSales() {
+        public Dictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ProductCategory))
                     dict[sale.ProductCategory] += sale.Amount;
                 else
                     dict[sale.ProductCategory] = sale.Amount;
-            }    
+            }
             return dict;
+        }
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+
+            return sales;
         }
     }
 }
