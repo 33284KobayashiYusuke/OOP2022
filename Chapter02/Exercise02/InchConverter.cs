7d25edfe09c488523db3644e7823d5fb2a67003d
33284祐介 小林 <33284@ojs.ac.jp>
2022-05-12 14:22:41 +0900

    問題2.2

---
+++ Chapter02/Exercise02/InchConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+
+   public static class InchConverter {
+        private const double retio = 0.0254; 
+      
+        // インチからメートルを求める(静的メソッド)
+        public static double ToMeter(double inch) {
+            return inch * retio;
+        }
+
+
+    }
+}
--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+
+            PrintInchToMeterList(1, 10);
         }
-    }
+        //インチからメートルへの対応表を出力
+        private static void PrintInchToMeterList(int start, int stop) {
+            //FeetConverter fc = new FeetConverter();
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
+            }
+        }
+   }
 }
