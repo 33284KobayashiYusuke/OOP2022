39d58ba71b7c323565f4bc22dc3f4706027b01de
33284祐介 小林 <33284@ojs.ac.jp>
2022-05-13 15:55:18 +0900

    問題3.1

---
+++ Chapter03/Execise01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execise01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            Execise1_1(numbers);
+            Console.WriteLine("------------");
+
+            Execise1_2(numbers);
+            Console.WriteLine("------------");
+
+        }
+        private static void Execise1_1(List<int> numbers) {
+            var exists = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            Console.WriteLine(exists);
+        }
+        private static void Execise1_2(List<int> numbers) {
+            numbers.ForEach(n => Console.WriteLine(n/2.0));
+        }
+
+    }
+}
+    
