3c3bf2bffec3f370c33baecf9fc8268af26bbb3a
33284祐介 小林 <33284@ojs.ac.jp>
2022-05-26 15:25:24 +0900

    5.1

--- Chapter05/Execise01/Program.cs
+++ Chapter05/Execise01/Program.cs
@@ -7,7 +7,19 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
+            //5.1
+            Console.Write("文字列１を入力：");
+            var inputString1 = Console.ReadLine();
 
+            Console.Write("文字列２を入力：");
+            var inputString2 = Console.ReadLine();
+
+
+            if (inputString1.ToUpper() == inputString2.ToUpper()) {
+                Console.WriteLine("一致しています");
+            } else {
+                Console.WriteLine("一致していません");
+            }
         }
     }
 }
