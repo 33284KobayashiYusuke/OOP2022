6b2780b70bb44d041cba32b1cc6bc2e1c162a3e1
33284祐介 小林 <33284@ojs.ac.jp>
2022-05-31 15:48:39 +0900

    6章1.1-1.2完了

---
+++ Chapter06/Execise01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execise01 {
+    class Program {
+        static void Main(string[] args) {
+           
+                var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+                Exercise1_1(numbers);
+                Console.WriteLine("-----");
+
+                Exercise1_2(numbers);
+                Console.WriteLine("-----");
+
+                Exercise1_3(numbers);
+                Console.WriteLine("-----");
+
+                Exercise1_4(numbers);
+                Console.WriteLine("-----");
+
+                Exercise1_5(numbers);
+            }
+
+        private static void Exercise1_1(int[] numbers) {
+            var max = numbers.Max();
+            Console.WriteLine(max);
+        }
+
+        private static void Exercise1_2(int[] numbers) {
+            foreach (var num in numbers.Skip(9)) {
+                Console.WriteLine(num);
+            }
+        }
+
+        private static void Exercise1_3(int[] numbers) {
+         
+        }
+
+        private static void Exercise1_4(int[] numbers) {
+
+        }
+
+        private static void Exercise1_5(int[] numbers) {
+        }
+    }
+    
+}
