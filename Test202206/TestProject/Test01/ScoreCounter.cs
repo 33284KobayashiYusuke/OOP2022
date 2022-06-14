bd378fffa16b5be2e34cfc44bc0110ba6241f82d
33284祐介 小林 <33284@ojs.ac.jp>
2022-06-14 14:50:35 +0900

    Test1　完了

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -8,32 +8,43 @@ namespace Test01 {
         // コンストラクタ
         public ScoreCounter(string filePath) {
 
+            _score = ReadScore(filePath);
 
-
-            
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+            List<Student> score = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student scores = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                score.Add(scores);
+            }
 
+            return score;
 
-
-
-
-
-            
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
 
-
-
-
-
-            
+            var dict = new Dictionary<string, int>();
+            foreach (var scores in _score) {
+                if (dict.ContainsKey(scores.Subject))
+                    dict[scores.Subject] += scores.Score;
+                else
+                    dict[scores.Subject] = scores.Score;
+            }
+            return dict;
         }
+
+
     }
+    
 }
