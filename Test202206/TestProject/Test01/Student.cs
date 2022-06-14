19a96cf5a644c991203379864920d8fe062bd7d3
33284祐介 小林 <33284@ojs.ac.jp>
2022-06-14 14:22:56 +0900

    クラスStudent　完了

--- Test202206/TestProject/Test01/Student.cs
+++ Test202206/TestProject/Test01/Student.cs
@@ -1,6 +1,9 @@
 ﻿namespace Test01 {
     class Student {
 
+       public static string Name { get; set; }//学生の名前
+       public static string Subject { get; set; }//科目名
+       public static int Score { get; set; }//点数
 
 
     }
