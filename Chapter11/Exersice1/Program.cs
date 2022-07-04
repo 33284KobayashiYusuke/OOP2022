3b09ed9fb3c835cd8d542edcf8ffb20193bee057
33284祐介 小林 <33284@ojs.ac.jp>
2022-07-04 11:57:15 +0900

    Exercise １＿２　途中経過

---
+++ Chapter11/Exersice1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+
+
+namespace Exersice1 {
+    class Program {
+        static void Main(string[] args) {
+
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+        }
+
+        private static void Exercise1_1(string file) {
+            var xdoc = XDocument.Load("sample.xml");
+            var BallSports = xdoc.Root.Elements().Select(x => new {
+                Name = (string)x.Element("name"),
+                Members = (string)x.Element("teammembers")
+            });
+            foreach(var date in BallSports) {
+                Console.WriteLine("{0} {1}",date.Name,date.Members);
+            }
+        }
+        private static void Exercise1_2(string file) {
+            var xdoc = XDocument.Load("sample.xml");
+            var BallSports = xdoc.Root.Elements().Select(x => new {
+                FirstPlayed = (string)x.Element("firstplayed"),
+                Kanji = (string)x.Element("kanji")
+            });
+            foreach (var date in BallSports.OrderBy(x => x)) {
+                Console.WriteLine("{0} ({1})",date.FirstPlayed,date.Kanji);
+            }
+        }
+        private static void Exercise1_3(string file) {
+
+        }
+
+        private static void Exercise1_4(string file, string newfile) {
+
+        }
+    }
+}
--- Chapter11/XmlSample/Program.cs
+++ Chapter11/XmlSample/Program.cs
@@ -9,21 +9,16 @@ using System.Net;
 namespace XmlSample {
     class Program {
         static void Main(string[] args) {
-
-            using (var wc = new WebClient()) {
-
-
-                var stream = wc.OpenRead("https://news.yahoo.co.jp/rss/media/biz_lifeh/all.xml");
-
-                var xdoc = XDocument.Load(stream);
-              
-                var xTitle = xdoc.Root.Descendants("item").Select(x=>x.Element("title"));
-
-
-                foreach (var data in xTitle) {
-                    Console.WriteLine(data);
-                }
-                wc.Dispose();
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death")
+                                });
+            foreach (var novelist in novelists) {
+                Console.WriteLine("{0} ({1}-{2})",
+                                   novelist.Name, novelist.Birth.Year, novelist.Death.Year);
             }
         }
     }
