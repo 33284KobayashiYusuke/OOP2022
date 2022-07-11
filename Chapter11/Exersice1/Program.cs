6802af6363f5c047c114b0b85bafa8f5140f2696
33284祐介 小林 <33284@ojs.ac.jp>
2022-07-11 11:08:45 +0900

    Exercise01　完了

--- Chapter11/Exersice1/Program.cs
+++ Chapter11/Exersice1/Program.cs
@@ -12,6 +12,7 @@ namespace Exersice1 {
         static void Main(string[] args) {
 
             var file = "sample.xml";
+            
             Exercise1_1(file);
             Console.WriteLine();
             Exercise1_2(file);
@@ -21,6 +22,9 @@ namespace Exersice1 {
 
             var newfile = "sports.xml";
             Exercise1_4(file, newfile);
+
+            var text = File.ReadAllText(newfile);
+            Console.WriteLine(text);
         }
 
         private static void Exercise1_1(string file) {
@@ -37,17 +41,40 @@ namespace Exersice1 {
             var xdoc = XDocument.Load("sample.xml");
             var BallSports = xdoc.Root.Elements().Select(x => new {
                 FirstPlayed = (string)x.Element("firstplayed"),
-                Kanji = (string)x.Element("kanji")
+                Kanji = (string)x.Element("name").Attribute("kanji")
             });
-            foreach (var date in BallSports.OrderBy(x => x)) {
+            foreach (var date in BallSports.OrderBy(x => x.FirstPlayed)) {
                 Console.WriteLine("{0} ({1})",date.FirstPlayed,date.Kanji);
             }
         }
         private static void Exercise1_3(string file) {
-
+            var xdoc = XDocument.Load("sample.xml");
+            var BallSports = xdoc.Root.Elements().Select(x => new {
+                Name = (string)x.Element("name"),
+                Members = (string)x.Element("teammembers")
+            }).OrderByDescending(x => int.Parse(x.Members)).First();
+            Console.WriteLine("{0} ({1}人)", BallSports.Name, BallSports.Members);
         }
 
         private static void Exercise1_4(string file, string newfile) {
+            var element = new XElement("ballsports",
+                          new XElement("name", "サッカーボール",
+                          new XAttribute("kanji", "蹴球")),
+                          new XElement("teammembers", "11"),
+                          new XElement("firstplayed", "1863"));
+
+            var xdoc = XDocument.Load(file); 
+            xdoc.Root.Add(element);
+            xdoc.Save(newfile);
+           
+          
+
+
+
+
+
+
+
 
         }
     }
