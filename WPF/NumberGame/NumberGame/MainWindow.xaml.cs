c4d55df05c11ed8a6912249b1619bc8027f2a130
33284祐介 小林 <33284@ojs.ac.jp>
2022-10-24 15:01:14 +0900

    CollarChecker レイアウト　完了

---
+++ WPF/NumberGame/NumberGame/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberGame {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        Random rnd = new Random();
+
+        private int correctNumber;//正解ナンバー
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Button bt = (Button)sender;
+            if ((string)bt.Content == correctNumber.ToString()) {
+                textblock.Text = "正解！";
+                bt.Background = Brushes.Green;
+            }
+            else {
+                textblock.Text = int.Parse((string)bt.Content) < correctNumber
+                                                ?"もっと大きいです" : "もっと小さいです";
+                bt.Background = int.Parse((string)bt.Content) < correctNumber
+                                                ? Brushes.Blue : Brushes.Green;
+            }
+        }
+        private void Window_Loaded(object sender,RoutedEventArgs e) {
+            textblock.Text = "ゲームスタート";
+            correctNumber = rnd.Next(25) + 1; //正解ナンバーを設定
+            this.Title = correctNumber.ToString();
+        }
+    }
+}
--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,10 +20,64 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            seasonComboBox.SelectedIndex = getSeasonIndex();
+
+        }
+        private int getSeasonIndex() {
+            var today = DateTime.Now;
+            int ret = 0;
+
+            switch (today.Month) {
+                case 3:
+                case 4:
+                case 5:
+                    ret = 0;
+                    break;
+                case 6 :
+                case 7:
+                case 8:
+                    ret = 1;
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    ret = 2;
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    ret = 3;
+                    break;
+            }
+            return ret;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
             MessageBox.Show("ボタンが押された");
         }
+        private void checkBox_Checkd(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "チェック済み";
+        }
+        private void checkBox_Uncheckd(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "未チェック";
+        } 
+        private void seasonComboBox_SelectionChenged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
+
+        private void radRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "赤";
+        }
+
+        private void yellowRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "黄";
+        }
+
+        private void blueRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "青";
+        }
+
+      
+        
     }
 }
