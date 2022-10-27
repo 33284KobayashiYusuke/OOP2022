139292ddda336c6658c221fcfbee60aca7dce62a
33284祐介 小林 <33284@ojs.ac.jp>
2022-10-27 11:08:40 +0900

    CollarChecker ラベル背景色変わらない

--- WPF/CollarChecker/MainWindow.xaml.cs
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Drawing;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,13 +21,31 @@ namespace CollarChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+
         }
-        private void CollarCheck() {
-            byte R = (byte)sl1.Value;
-            byte G = (byte)sl2.Value;
-            byte B = (byte)sl3.Value;
-            Color color = Color.FromRgb(R, G, B);
+
+        private void ChangeCollar() {
+            byte rr = (byte)sl1.Value;
+            byte gg = (byte)sl2.Value;
+            byte bb = (byte)sl3.Value;
+            Color color = Color.FromRgb(rr, gg, bb);
+            SolidColorBrush colorBrush = new SolidColorBrush(color);
+            label1.Background = colorBrush;
         }
-        //private void  slider1_Value
+
+        private void sl1_ValueChanged(object sender,RoutedPropertyChangedEventArgs<double> e) {
+            ChangeCollar();
+        }
+        private void sl2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            ChangeCollar();
+        }
+        private void sl3_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            ChangeCollar();
+        }
+    }
+    public class MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; }
     }
 }
